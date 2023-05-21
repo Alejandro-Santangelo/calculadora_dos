07c5570e4dd4ac158767126937d2c46ffe4f747f
Alejandro-Santangelo <artesaniaslarustica@gmail.com>
2023-05-21 08:14:02 -0300

    modificando rachivo resta

--- resta.py
+++ resta.py
@@ -1,32 +1,7 @@
 
-import Resta
-import division
-import multiplicacion
-import suma
-
-operacion = input("Escriba una operación de entre estas (suma, resta, multiplicacion, division): ")
-
-a = float(input("Ingrese el primer valor: "))
-b = float(input("Ingrese el segundo valor: "))
-
 if operacion == "resta":
     resultado = Resta.resta(a, b)
     print("El resultado de la resta es:", resultado)
-
-elif operacion == "division":
-    resultado = division.division(a, b)
-    print("El resultado de la división es:", resultado)
-
-elif operacion == "multiplicacion":
-    resultado = multiplicacion.multiplicacion(a, b)
-    print("El resultado de la multiplicación es:", resultado)
-
-elif operacion == "suma":
-    resultado = suma.suma(a, b)
-    print("El resultado de la suma es:", resultado)
-
-else:
-    print("Operación no válida")
 
     
 
