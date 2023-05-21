cad387b26b96a56c0514d363459cc3dec3e020c6
Alejandro-Santangelo <artesaniaslarustica@gmail.com>
2023-05-21 08:49:48 -0300

    nuevo cambio

--- troncal1.py
+++ troncal1.py
@@ -3,7 +3,7 @@ import division
 import multiplicacion
 import suma
 
-calculo = input("Escriba una operación de entre estas (suma, resta, multiplicacion, division): ")
+calculo = str(input("Escriba una operación de entre estas (suma, resta, multiplicacion, division): "))
 
 a = float(input("Ingrese el primer valor: "))
 b = float(input("Ingrese el segundo valor: "))
